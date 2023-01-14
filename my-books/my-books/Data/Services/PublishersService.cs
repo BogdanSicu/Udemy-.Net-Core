c21f3e9e9f313fdb05e94dbf19856f6164c02b24
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2023-01-14 13:25:30 +0200

    Added authors and publishers services and controllers

---
+++ my-books/my-books/Data/Services/PublishersService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+using my_books.Data.Models;
+using my_books.Data.ViewModels;
+
+namespace my_books.Data.Services
+{
+    public class PublishersService
+    {
+        private AppDbContext _context;
+
+        public PublishersService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddPublisher(PublisherViewModel publisherVM)
+        {
+            var _publisher = new Publisher()
+            {
+                Name = publisherVM.Name 
+            };
+
+            _context.Publishers.Add(_publisher);
+            _context.SaveChanges();
+        }
+    }
+}
